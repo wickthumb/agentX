716854f9d4724e849e66a0659b2b03b4fd9a32f8
wickthumb <stevenbrown3214@hmail.com>
2024-11-21 21:06:11 -0600

    Update project name in publish.yml and setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='agentX',
+    name='agentx',
     version='0.1.0',
     author='wickthumb',
     author_email='steven@wick3d.llc',
